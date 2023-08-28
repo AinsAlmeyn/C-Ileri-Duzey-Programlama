081071d265a1576cb8ab30b788dc713cc1f8c21b
yıldıray koçak <kocakakay@gmail.com>
2023-08-28 14:33:01 +0300

    1432

--- IleriDuzeyProgramlama/Program.cs
+++ IleriDuzeyProgramlama/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+
 #region Deger tipli degiskenlerde ref keywordu calisiyor.
 //int b = 5;
 //x(ref b);
@@ -8,6 +9,15 @@
 //{
 //    a = 123;
 //}
+
+//string b = "deneme";
+//void x(string b) 
+//{
+//    b = "deger";
+//}
+//x(b);
+//Console.WriteLine(b);
+
 #endregion
 
 #region Referanst tipi nesnelerde Ref Keywordu Calisiyor.
@@ -42,7 +52,7 @@
 //Console.WriteLine(person.Name);
 //void nameConvert(Person person)
 //{
-//    person.Name = "Kocak";
+//person.Name = "Kocak";
 //}
 
 //class Person
@@ -210,13 +220,32 @@
 
 // structlar icerisindeki operatif tanimlamalari kullanmak icin new keywordunu kullanmak zorundayiz.
 
+
+//Boyut: Genellikle küçük veri yapıları için struct'ları tercih edin. Struct'lar yığın belleği üzerinde saklanır, bu yüzden büyük veri yapıları için uygun değillerdir çünkü yığın hızla dolabilir. Ayrıca, büyük bir struct'ı kopyalamak da maliyetlidir.
+
+//Ömür: Sık sık oluşturulup yok edilen kısa ömürlü nesneler için struct'ları tercih edebilirsiniz, çünkü yığın belleğindeki nesnelerin yönetimi genellikle yığın dışındakilere göre daha verimlidir.
+
+//Değişmezlik(Immutability): Değer tipleri genellikle değişmez olmalıdır. Eğer veri yapınızın değişmez olması önemliyse, struct kullanmayı düşünün.
+
+//Kimlik vs Değer: Bir nesnenin kimliği (identity) mi yoksa değeri mi önemli? Örneğin, iki Point nesnesi aynı koordinatlara sahipse aynı olarak kabul edilirler. Bu tür durumlar için struct kullanımı uygundur.Ancak, iki farklı öğrenci nesnesi aynı bilgilere sahip olsa bile (isim, yaş, vs.) farklı kimlikleri vardır ve bu nedenle farklıdırlar. Bu tür durumlar için class kullanmalısınız.
+
+//Referans Semantiği: Bir nesneyi bir fonksiyona referans olarak geçirmeniz ve bu fonksiyonun nesneyi değiştirmesi gerekiyorsa, class kullanmanız daha uygundur.
+
+//Miraz(Inheritance): Eğer mirasa ihtiyaç duyuyorsanız, class kullanmalısınız. Struct'lar miras alınamaz.
+
+//Performans: Struct'lar bellekte ardışık olarak yerleştirilir ve küçük veri yapıları için cache dostudur. Ancak, bu avantaj genellikle sadece yoğun hesaplamalarda farkedilir.
+
+//Yeniden Atama ve Yan Etkiler: Struct'lar değer tipli olduğu için kopyalandığında tamamen yeni bir kopya oluşturulur. Bu, yanlışlıkla bir struct'ı değiştirme riskini ortadan kaldırabilir. Ancak, bu da bazen beklenmedik sonuçlara yol açabilir, bu yüzden dikkatli olmalısınız.
+
+//Null Değerler: Struct'lar null değer alamazken, class'lar null değer alabilir. Eğer bir veri yapınızın null olabileceğini düşünüyorsanız, class kullanmanız daha iyi olabilir.
 //using System.Globalization;
+//using System.Diagnostics.Metrics;
 
 //MyStruct myStruct = new();
 //myStruct.Deneme(myStruct.a);
 
-//MyStruct? myStruct = new();
-//myStruct.Value.Deneme(); // Nullable olarak tanimlarsan value uzerinden erismelisin
+////MyStruct? myStruct = new();
+////myStruct.Value.Deneme(); // Nullable olarak tanimlarsan value uzerinden erismelisin
 
 //public struct MyStruct
 //{
@@ -272,3 +301,5 @@ public record MyRecord
 }
 
 #endregion
+
+
