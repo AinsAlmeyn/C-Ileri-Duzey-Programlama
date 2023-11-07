11c93ed7948f81e01258a51eef6550e830ad050d
Yıldıray Koçak <100589440+AinsAlmeyn@users.noreply.github.com>
2023-11-08 00:32:48 +0300

    032

---
+++ RedisPubSubBroker.Subscriber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using StackExchange.Redis;
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453"
+    //,
+    //options =>
+    //{
+    //    options.SslHost = "",
+    //    options.Password = "",
+
+    //}
+    );
+ISubscriber subscriber = connection.GetSubscriber();
+await subscriber.SubscribeAsync("mychannel", (channel, message) =>
+{
+    global::System.Console.WriteLine(message);
+});
+
+//await subscriber.SubscribeAsync("stock.*", (channel, message) => // Coklu kanal kullanma ve filtreleme
+//{
+//    global::System.Console.WriteLine(message);
+//});
+
+Console.Read();
---
+++ RedisPubSubBroker/Program.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453"
+    //,
+    //options =>
+    //{
+    //    options.SslHost = "",
+    //    options.Password = "",
+
+    //}
+    );
+ISubscriber subscriber = connection.GetSubscriber();
+while (true)
+{
+    global::System.Console.Write("Mesaj :");
+    string mesaj = Console.ReadLine();
+    await subscriber.PublishAsync("mychannel", mesaj);
+}
